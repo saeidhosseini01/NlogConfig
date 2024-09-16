9cabcbe5906085e2a9810e74a722b00c48f0c856
SeyedsaeidHosseini <SeyedsaeidHosseini1990@gmail.com>
2024-09-16 07:37:39 +0430

    jsonelog

--- WebApplication1/Pages/Index.cshtml.cs
+++ WebApplication1/Pages/Index.cshtml.cs
@@ -6,6 +6,20 @@ namespace WebApplication1.Pages
 
 
 
+
+
+
+    public class persone
+    {
+        public string name { get; set; }
+        public string family { get; set; }
+        public string tell { get; set; }
+        public override string ToString()
+        {
+            return name + " " + family;
+        }
+    }
+
     public class IndexModel : PageModel
     {
         private readonly ILogger<IndexModel> _logger;
@@ -18,10 +32,14 @@ namespace WebApplication1.Pages
 
         public void OnGet()
         {
-            for (int i = 0; i < 2500000; i++)
-            {
-                _logger.LogInformation("dddddddddddd");
-            }
+            persone persone = new persone()
+            { 
+                name="saeed",
+                family="hosseini",
+                tell="09159524625"
+            };
+            _logger.LogInformation("This Is A test {persone}", persone);
         }
+     
     }
 }
